b02801aca28fd6c0a61f181a5919e684676133f3
qiushaohua <qiushaohua@yahoo.cn>
2012-03-23 16:24:13 +0800

    change the dialog title, used i18n text instead

--- package-dispatch/package-dispatch-webapp/src/main/webapp/js/qiuq/order/new.js
+++ package-dispatch/package-dispatch-webapp/src/main/webapp/js/qiuq/order/new.js
@@ -93,7 +93,7 @@ define([
     function popupReceiverList() {
         initReceiverTable({}, false).then(function(html) {
             new Dialog({
-                "title" : "Receiver",
+                "title" : message["receiver"],
                 "content" : html,
                 style : {
                     width : "600px",
